d5d66d706af714b9bafa2acc5f9864e05f0688fd
cools <cools@DESKTOP-U6U820H>
2016-11-24 02:42:31 -0500

    index.html -> index.jsp, better sql query for login

--- src/wf/LoginServlet.java
+++ src/wf/LoginServlet.java
@@ -23,9 +23,10 @@ public class LoginServlet extends HttpServlet {
     private static String dbUsername = "cse305";
     private static String dbPassward = "cse305";
     private static String connectionString = "jdbc:mysql://localhost:3306/withfriends?autoReconnect=true&useSSL=false";
+    private String query;
     private static Connection connection;
     private static Statement command;
-    private static ResultSet data;
+    private ResultSet data;
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -45,24 +46,16 @@ public class LoginServlet extends HttpServlet {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
             connection = DriverManager.getConnection(connectionString, dbUsername, dbPassward);
             command = connection.createStatement();
-            data = command.executeQuery("SELECT EmailID, PWD FROM withfriends_users"); 
+            query = "SELECT EmailID, PWD FROM withfriends_users WHERE EmailId = '" + inputUname + "' AND PWD = '" + inputPWD + "'";
+            data = command.executeQuery(query); 
+            if (data.next()) {
+                System.out.println("Horray");
+            } else {
+                System.out.println("Boo");
+            }
         } catch(Exception e) {
             e.printStackTrace();
-        } finally {
-            try {
-                while (data.next()) {
-                    String currUname = data.getString(1);
-                    String currPWD = data.getString(2);
-                    if (inputUname.equals(currUname) && inputPWD.equals(currPWD)) {
-                        System.out.println(data.getString(1) + " " + data.getString(2));
-                    } else {
-                        System.out.println("No account");
-                    }
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            } 
-        }  
+        } 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
--- web/index.jsp
+++ web/index.jsp
@@ -1,14 +1,21 @@
-<%--
-  User: Yida Yuan
-  Date: 11/21/16
-  Time: 2:16 AM
---%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-  <head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <script src="wfscript.js"></script>
     <title>WithFriends</title>
-  </head>
-  <body>
-  $END$
-  </body>
+</head>
+<body>
+    <form  action="login"  method="POST">
+    <span class="inputText" >User ID:</span>
+    <input id="uname" name="username" type="text" />
+    &nbsp;
+    <span class="inputText" >Password:</span>
+    <input id="pwd" name="password" type="password" />
+    &nbsp;
+    <input id="loginBt" type="submit" value="Log In" />
+    &nbsp;
+    <input id="SignUpBt" type="button" value="Sign Up" onclick="SignUpBt_onclick()" />
+</form>
+</body>
 </html>
