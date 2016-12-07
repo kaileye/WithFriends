d301e1ca92e2f978c32a77ebbced7a48031fbf5b
csha <danielsha94@gmail.com>
2016-12-07 17:04:54 -0500

    Create an advertisement (servlet)
    Create an advertisement

---
+++ src/wf/CreateAdvertisementServlet.java
@@ -0,0 +1,51 @@
+package wf;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import wf.userbean.User;
+
+@WebServlet(name = "CreateAdvertisementServlet", urlPatterns = {"/createad"})
+public class CreateAdvertisementServlet extends HttpServlet {
+
+    private static String dbUsername = "cse305";
+    private static String dbPassward = "cse305";
+    private static String connectionString = "jdbc:mysql://localhost:3306/withfriends?autoReconnect=true&useSSL=false";
+    private static Connection connection;
+    private ResultSet data;
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        User u = (User)session.getAttribute("user");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(connectionString, dbUsername, dbPassward);
+            PreparedStatement ps = connection.prepareStatement("INSERT INTO advertisements(EmployeeId, Type, Company, ItemName, Content, UnitPrice) VALUES(?,?,?,?,?,?)");
+            ps.setString(1, u.getUserId());
+            ps.setString(2, request.getParameter("createadtype"));
+            ps.setString(3, request.getParameter("createadcompany"));
+            ps.setString(4, request.getParameter("createadname"));
+            ps.setString(5, request.getParameter("createadcontent"));
+            ps.setString(6, request.getParameter("createadprice"));
+            ps.executeUpdate();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            RequestDispatcher rs = request.getRequestDispatcher(request.getParameter("pg"));
+            rs.forward(request, response);
+        }
+    }
+}
